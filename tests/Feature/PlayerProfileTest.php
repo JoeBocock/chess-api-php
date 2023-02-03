14c94850fe193b680bdf247474c64b7880d919c9
Joe Bocock <joebocock@gmail.com>
2023-02-03 18:35:56 +0000

    [PRO-3] Refactor existing test

--- tests/Feature/PlayerProfileTest.php
+++ tests/Feature/PlayerProfileTest.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 use GuzzleHttp\Psr7\Response;
-use JoeBocock\ChessApi\Chess;
 use JoeBocock\ChessApi\Entities\PlayerProfile;
 
 it('fetches a players profile', function () {
-    $chess = new Chess(mockClient([
-        new Response(body: file_get_contents('./tests/fixtures/PlayerProfileResponse.json', true)),
-    ]));
+    $chess = mockClient([
+        new Response(body: file_get_contents('./tests/Fixtures/PlayerProfileResponse.json', true)),
+    ]);
 
     expect($chess->playerProfile('username'))
         ->toBeInstanceOf(PlayerProfile::class);
